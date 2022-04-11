d4511621490204049116a8a9f21d2065ce863458
Meesam Amir <86166187+meesamamir@users.noreply.github.com>
2022-04-11 15:47:38 -0500

    commit 2 - meesam

--- main.py
+++ main.py
@@ -3,3 +3,16 @@
 #1wasd
 
 # meesam 
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "asdjsdjkacjksdc" #encrypts cookies and session data related to website, it can be whatever we want
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # update flask server with changes we make
