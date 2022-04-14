bdd25ed4ea67f03d259b21fb9203ee6989b1c1bf
Meesam Amir <86166187+meesamamir@users.noreply.github.com>
2022-04-14 18:17:11 -0500

    commit - meesam

--- main.py
+++ main.py
@@ -10,11 +10,37 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "asdjsdjkacjksdc" #encrypts cookies and session data related to website, it can be whatever we want
 
-@app.route("/jessie")
+@app.route("/")
 def home():
+    return render_template("home.html")
+
+@app.route("/tri")
+def tri():
+    return render_template("tri.html")
+
+@app.route("/meesam")
+def meesam():
+    return render_template("meesam.html")
+
+@app.route("/som")
+def som():
+    return render_template("som.html")
+
+@app.route("/jessie")
+def jessie():
     return render_template("jessie.html")
+    
+
+    
+
+
+
+    
 
 
 if __name__ == '__main__':
     app.run(debug=True)  # update flask server with changes we make
+
+
+
 # team kiwi
